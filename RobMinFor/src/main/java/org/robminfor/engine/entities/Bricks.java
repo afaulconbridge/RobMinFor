6d705642ae7fa5fde530fb8a38d9a1b29c5a5f47
Adam Faulconbridge <afaulconbridge@googlemail.com>
2013-04-26 17:05:11 +0100

    Added bricks and stonemasonry entities

---
+++ RobMinFor/src/main/java/org/robminfor/engine/entities/Bricks.java
@@ -0,0 +1,20 @@
+package org.robminfor.engine.entities;
+
+public class Bricks extends AbstractEntity {
+	
+	private static final Bricks singleton = new Bricks();
+	
+	protected Bricks() {
+		super();
+	}
+	
+	public static Bricks getInstance(){
+		return singleton;
+	}
+
+	@Override
+	public boolean isSolid() {
+		return true;
+	}
+
+}
---
+++ RobMinFor/src/main/java/org/robminfor/engine/entities/Stonemasonry.java
@@ -0,0 +1,9 @@
+package org.robminfor.engine.entities;
+
+public class Stonemasonry extends AbstractFacility {
+
+	public Stonemasonry() {
+		super();
+	}
+
+}
