7c23c8b1307cccb80425cb357becdfdf31430b11
Adam Faulconbridge <afaulconbridge@googlemail.com>
2013-06-18 17:09:37 +0100

    Added true equals method

--- RobMinFor-core/src/main/java/org/robminfor/util/Vect.java
+++ RobMinFor-core/src/main/java/org/robminfor/util/Vect.java
@@ -22,4 +22,17 @@ public class Vect {
 	public int getZ(){
 		return z;
 	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if (other == null) return false;
+		if (other == this) return true;
+		if (!this.getClass().isInstance(other)) return false;
+		Vect o = (Vect) other;
+		if (o.getX() != this.getX()) return false;
+		if (o.getY() != this.getY()) return false;
+		if (o.getZ() != this.getZ()) return false;
+		return true;
+	}
+	
 }
